ce79b0c41b0117d5e399b9706c3a30d0f7d4c971
imnct0805 <imnct0805@gmail.com>
2018-08-05 21:19:31 +0900

    BOJ 15857 Basic

---
+++ BOJ/ETC/Basic/15857.cpp
@@ -0,0 +1,21 @@
+#include <cstdio>
+int main() {
+	int ans;
+	scanf("%d", &ans);
+	if (ans == 1)
+		printf("a\n");
+	else if (ans == 2)
+		printf("b\n");
+	else if (ans == 3)
+		printf("b\n");
+	else if (ans == 4)
+		printf("c\n");
+	else if (ans == 5)
+		printf("d\n");
+	else if (ans == 6)
+		printf("d\n");
+	else if (ans == 7)
+		printf("d\n");
+	else if (ans == 8)
+		printf("c\n");
+}
