fcb39dd8fc49b002666884ee463474b87acb43e5
imnct0805 <imnct0805@gmail.com>
2018-08-06 16:33:36 +0900

    BOJ 2750 Algorithm

---
+++ BOJ/ETC/Algorithm/2750.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+#include <algorithm>
+using namespace std;
+int main() {
+	int n, d[1001];
+	cin >> n;
+	for (int i = 0; i < n; ++i)
+		scanf("%d", &d[i]);
+	sort(d, d + n);
+	for (int i = 0; i < n; ++i)
+		printf("%d\n", d[i]);
+}
