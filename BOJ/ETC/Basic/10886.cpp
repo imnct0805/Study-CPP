2bff7b0fb19a48b44a3fd5ba557021dbdabb8558
imnct0805 <imnct0805@gmail.com>
2018-08-06 16:03:52 +0900

    BOJ 10886 Basic

---
+++ BOJ/ETC/Basic/10886.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+using namespace std;
+int main() {
+	int n, cute = 0, ncute = 0;
+	cin >> n;
+	while (n--) {
+		int num;
+		cin >> num;
+		if (num == 1)			cute++;
+		else			ncute++;
+	}
+	if (cute > ncute)		printf("Junhee is cute!\n");
+	else		printf("Junhee is not cute!\n");
+}
