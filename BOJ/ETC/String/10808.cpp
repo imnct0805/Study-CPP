3dad4c4fadabce025e158da9901ddb6c86a25944
imnct0805 <imnct0805@gmail.com>
2018-08-06 17:50:42 +0900

    BOJ 10808 String

---
+++ BOJ/ETC/String/10808.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+int main() {
+	int d[26] = { 0, };
+	string str;
+	cin >> str;
+	for (int i = 0; i < str.length(); ++i) 
+		d[str[i] - 97]++;
+	for (int i = 0; i < 26; ++i)
+		printf("%d ", d[i]);
+}
