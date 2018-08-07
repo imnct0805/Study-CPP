b5563f94b2ab39e8f58d0c3f7025a4b63f0e17c4
imnct0805 <imnct0805@gmail.com>
2018-08-07 15:36:26 +0900

    BOJ 15873 String

---
+++ BOJ/ETC/String/15873.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+#include <string.h>
+using namespace std;
+int main() {
+	char c[5];
+	scanf("%s", c);
+	int len = strlen(c);
+	if (len == 2)
+		cout << (atoi(c)  / 10) + (atoi(c) % 10) << endl;
+	else if (len == 3) {
+		if (c[1] == '0')
+			cout << ((atoi(c) / 100) * 10 + (atoi(c) % 100 / 10)) + (atoi(c) % 10) << endl;
+		else
+			cout << ((atoi(c) / 100) + ((atoi(c) % 100 / 10) * 10 + (atoi(c) % 10))) << endl;
+	}
+	else if (len == 4)
+		cout << 20 << endl;
+}
