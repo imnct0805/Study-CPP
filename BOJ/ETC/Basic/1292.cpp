5dd7412de7f48cfd1c0fb29903002c6d992d9424
imnct0805 <imnct0805@gmail.com>
2018-08-07 15:45:26 +0900

    BOJ 1292 Basic

---
+++ BOJ/ETC/Basic/1292.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+using namespace std;
+
+int d[1001];
+int sequence() {
+	int idx = 1, cnt = 0;
+	for (int i = 1; i < 1001; ++i) {
+		for (int j = 1; j < i; j++) {
+			d[idx++] = cnt;
+			if (idx == 1001)	return 0;
+		}
+		cnt++;
+	}
+}
+
+int main() {
+	sequence();
+	int a, b, sum = 0;
+	cin >> a >> b;
+	for (int i = a; i <= b; ++i)
+		sum += d[i];
+	printf("%d\n", sum);
+}
